220adb0a56d7f9ea1c319cb54d59df18ee6d5298
ssm123ssm <ssm123ssm@gmail.com>
2019-06-23 13:31:12 +0530

    fix in main js in loading background image

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -510,7 +510,7 @@ function setBackground() {
 
             if (data.currentVersion) {
                 console.log('Connection established');
-                $("body").css('background-image', `url('./res/blobs/${currentImage}.jpg')`);
+                $("body").css('background-image', `url('https://mixedcnt.herokuapp.com/get?url=http://35.200.245.209:8080/res/blobs/${currentImage}.jpg')`);
                 var newCache = Cookies.getJSON('imageCache').images;
                 if (newCache.indexOf(currentImage) < 0) {
                     newCache.push(currentImage);
